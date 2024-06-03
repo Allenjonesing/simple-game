4c143524b242fb0088465c4bd5abcaa814f7c28d
Allenjonesing <Allenjonesing@gmail.com>
2024-06-03 14:58:26 -0500

    5

--- main.js
+++ main.js
@@ -23,19 +23,21 @@ function preload() {
         console.log('Assets loaded successfully.');
     });
 }
-    
+
 function create() {
-    const client = new Photon.LoadBalancing.LoadBalancingClient(Photon.ConnectionProtocol.Wss, "fdd578f2-f3c3-4089-bcda-f34576e0b095", "1.0");
+    console.log('Creating game scene...');
+    client = new Photon.LoadBalancing.LoadBalancingClient(Photon.ConnectionProtocol.Wss, "fdd578f2-f3c3-4089-bcda-f34576e0b095", "1.0");
 
     client.onStateChange = function(state) {
         console.log("State:", state);
     };
 
     client.onEvent = (code, content, actorNr) => {
+        console.log("Event received:", code, content);
         if (code === 1) {
             if (content.playerId !== player.id) {
                 if (!otherPlayers[content.playerId]) {
-                    otherPlayers[content.playerId] = createOtherPlayer(content.x, content.y);
+                    otherPlayers[content.playerId] = createOtherPlayer(content.x, content.y, this.scene);
                 } else {
                     otherPlayers[content.playerId].setPosition(content.x, content.y);
                 }
@@ -44,11 +46,56 @@ function create() {
     };
 
     client.onJoinRoom = () => {
-        player = createPlayer();
+        console.log("Joined room");
+        player = createPlayer(this);
         client.raiseEvent(1, { playerId: player.id, x: player.x, y: player.y });
     };
 
+    client.onError = function(errorCode, errorMsg) {
+        console.log(`Error: ${errorCode} - ${errorMsg}`);
+    };
+
+    client.onRoomListUpdate = function(rooms) {
+        console.log("Room list updated:", rooms);
+    };
+
+    client.onRoomList = function(rooms) {
+        console.log("Room list received:", rooms);
+    };
+
+    client.onJoinRoomFailed = function(errorCode, errorMsg) {
+        console.log(`Failed to join room: ${errorCode} - ${errorMsg}`);
+        // Attempt to create a room if joining failed
+        createRoom();
+    };
+
+    client.onConnectedToMaster = function() {
+        console.log("Connected to master server");
+        client.joinRandomRoom();
+    };
+
+    function createRoom() {
+        const roomOptions = {
+            isVisible: true,
+            isOpen: true,
+            maxPlayers: 10
+        };
+        client.createRoom("myTestRoom", roomOptions);
+    }
+
     client.connectToRegionMaster("us");
+
+    // Add some static objects
+    this.add.text(10, 10, 'Welcome to the game!', { font: '16px Arial', fill: '#ffffff' });
+
+    // Add some animated objects
+    const star = this.add.star(400, 300, 5, 20, 40, 0xffffff, 0.5);
+    this.tweens.add({
+        targets: star,
+        rotation: 2 * Math.PI,
+        duration: 4000,
+        repeat: -1
+    });
 
     this.input.on('pointermove', pointer => {
         if (player) {
@@ -62,12 +109,15 @@ function update() {
     // Game loop logic here
 }
 
-function createPlayer() {
-    const playerSprite = this.add.image(400, 300, 'player');
+function createPlayer(scene) {
+    const playerSprite = scene.add.image(400, 300, 'player');
+    playerSprite.id = client.myActor().actorNr; // Assign an ID to the player sprite
     console.log('Player created:', playerSprite);
     return playerSprite;
 }
 
-function createOtherPlayer(x, y) {
-    return this.add.image(x, y, 'player');  // Ensure the 'this' context is correct
+function createOtherPlayer(x, y, scene) {
+    const otherPlayerSprite = scene.add.image(x, y, 'player');
+    console.log('Other player created:', otherPlayerSprite);
+    return otherPlayerSprite;
 }
