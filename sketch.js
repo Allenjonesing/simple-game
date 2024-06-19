3acb38c3dd2b6f62800b430d0cedc12c936b283c
Allenjonesing <Allenjonesing@gmail.com>
2024-06-19 15:20:07 -0500

    asd

--- sketch.js
+++ sketch.js
@@ -295,10 +295,9 @@ async function generateAIResponses(newsData, personas, setting) {
                         throw new Error('Network response was not ok');
                     }
 
-                    const responseText = await response.text(); // Read response as text first
-        const data = JSON.parse(responseText); // Parse the text as JSON
-        const parsedBody = JSON.parse(data.body); // Parse the body string as JSON
-        if (parsedBody && parsedBody.base64_image) {
+                    const data = await imageResponse.json();
+                    const parsedBody = JSON.parse(data.body);
+                    if (parsedBody && parsedBody.base64_image) {
                         console.log('generateEnemyImage... base64_image: ', parsedBody.base64_image);
                         return parsedBody.base64_image;
                     } else {
